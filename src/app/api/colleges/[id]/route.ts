8ca9ce07706273f2a0c2980bf4583ca7f77a64fb
iamthevivek <viveksarode63@gmail.com>
2026-05-25 23:42:34 +0530

    fix params type

--- src/app/api/colleges/[id]/route.ts
+++ src/app/api/colleges/[id]/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
-
 export async function GET(
   _: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params
     const college = await prisma.college.findUnique({
-      where: { id: params.id },
+      where: { id },
     })
     if (!college) {
       return NextResponse.json({ error: "Not found" }, { status: 404 })
